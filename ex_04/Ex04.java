be52964b60236be879f0458cfcff7351da58a723
simon marchal <simon.marchal@epitech.eu>
2023-10-24 05:05:12 -0400

    update

--- ex_04/Ex04.java
+++ ex_04/Ex04.java
@@ -3,10 +3,10 @@ public class Ex04 {
     public static void printMovieFromNbr(int nbr){
         switch (nbr) {
             case 3:
-                System.out.println("The Tree Brothers");
+                System.out.println("The Three Brothers");
                 break;
             case 6:
-                System.out.println("The sixth Sense");
+                System.out.println("The Sixth Sense");
                 break;
             case 23:
                 System.out.println("The Number 23");
