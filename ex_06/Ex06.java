4360d2c3d553f174af16b305c92697a05c268035
simon marchal <simon.marchal@epitech.eu>
2023-10-24 09:14:58 -0400

    update

--- ex_06/Ex06.java
+++ ex_06/Ex06.java
@@ -1,24 +1,29 @@
+import java.util.ArrayList;
 
 public class Ex06 {
     public static void sequence(int nbr){
-        String str = "1";
-        for (int i = 0; i < nbr; i++) {
-            System.out.println(str);
-            String tmp = "";
+        if (nbr < 0) {
+            return;
+        }
+        ArrayList<String> cauet = new ArrayList<String>();
+        cauet.add("1");
+        for(int i = 0; i < nbr; i++){
+            String current = cauet.get(i);
+            StringBuilder next = new StringBuilder();
             int count = 1;
-            for (int j = 0; j < str.length(); j++) {
-                if (j+1 < str.length() && str.charAt(j) == str.charAt(j+1)) {
+            for(int j = 0; j < current.length(); j++){
+                if(j + 1 < current.length() && current.charAt(j) == current.charAt(j + 1)){
                     count++;
                 } else {
-                    tmp += count;
-                    tmp += str.charAt(j);
+                    next.append(count).append(current.charAt(j));
                     count = 1;
                 }
             }
-            str = tmp;
+            cauet.add(next.toString());
+        }
+
+        for (String s : cauet) {
+            System.out.println(s);
         }
     }
-//    public static void main ( String [] args ) {
- //       sequence(10);
-   // }
 }
